edfebeb7dc45a921fd11ff35f05f4f968895fedb
veera sekhar <115022648+VEERASEKHAR-GIT@users.noreply.github.com>
2023-09-07 23:44:28 -0500

    Update python_loops.py

--- python_loops.py
+++ python_loops.py
@@ -1,1 +1,29 @@
 
+# Loops and Iterations with Different Variables and Data
+
+# List of names
+names = ['Alice', 'Bob', 'Charlie', 'David']
+
+# Simple for loop
+for name in names:
+    print(f'Hello, {name}!')
+
+# Break statement
+for name in names:
+    if name == 'Charlie':
+        break
+    print(f'Hi, {name}!')
+
+# Continue statement
+for name in names:
+    if name == 'Bob':
+        continue
+    print(f'Hey, {name}!')
+
+
+
+# While loop
+count = 0
+while count < 5:
+    print(f'Count: {count}')
+    count += 1
