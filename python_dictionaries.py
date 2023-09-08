fca257187d42e0c3583f4d32cb9163694cf29ac7
veera sekhar <115022648+VEERASEKHAR-GIT@users.noreply.github.com>
2023-09-07 23:40:47 -0500

    Update python_dictionaries.py

--- python_dictionaries.py
+++ python_dictionaries.py
@@ -25,3 +25,21 @@ print(employee.items())
 employee_copy = employee.copy()
 employee_copy['position'] = 'Manager'
 
+# Remove items
+del employee['department']
+employee.pop('email')
+
+# Clear the dictionary
+employee.clear()
+
+# Get the length of the copied dictionary
+print(len(employee_copy))
+
+# List of dictionaries
+employees = [
+    {'employee_id': 'E12345', 'first_name': 'Alice', 'last_name': 'Smith'},
+    {'employee_id': 'E67890', 'first_name': 'Bob', 'last_name': 'Johnson'}
+]
+
+print(employees[1]['last_name'])
+
